0132b0609c79133ed6057776f14f03accdeffda2
rakshitsaxena <saxena.rakshit@gmail.com>
2016-11-06 21:49:41 +0530

    add logic to support change in orientation in edit view

--- app/src/main/java/com/rakshitsaxena/notebook/NoteEditFragment.java
+++ app/src/main/java/com/rakshitsaxena/notebook/NoteEditFragment.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -18,9 +20,11 @@ import android.widget.ImageButton;
  */
 public class NoteEditFragment extends Fragment {
 
+    private static final String MODIFIED_CATEGORY = "Modified Category";
     private ImageButton noteCatButton;
     private Note.Category savedButtonCategory;
-    private AlertDialog categoryDialog;
+    private EditText title, message;
+    private AlertDialog categoryDialog, confirmDialog;
 
     public NoteEditFragment() {
         // Required empty public constructor
@@ -30,21 +34,39 @@ public class NoteEditFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        if(savedInstanceState != null){
+            savedButtonCategory = (Note.Category)savedInstanceState.get(MODIFIED_CATEGORY);
+        }
         // Inflate the layout for this fragment
         View framentLayout = inflater.inflate(R.layout.fragment_note_edit, container, false);
 
-        EditText title = (EditText) framentLayout.findViewById(R.id.editNoteTitle);
-        EditText message = (EditText) framentLayout.findViewById(R.id.editNoteMessage);
+        title = (EditText) framentLayout.findViewById(R.id.editNoteTitle);
+        message = (EditText) framentLayout.findViewById(R.id.editNoteMessage);
         noteCatButton = (ImageButton) framentLayout.findViewById(R.id.editNoteButton);
+        Button savedButton = (Button) framentLayout.findViewById(R.id.saveNote);
+
+        savedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirmDialog.show();
+            }
+        });
 
         Intent intent = getActivity().getIntent();
         title.setText(intent.getExtras().getString(MainActivity.NOTE_TITLE_EXTRA));
         message.setText(intent.getExtras().getString(MainActivity.NOTE_MESSAGE_EXTRA));
 
-        Note.Category noteCat = (Note.Category) intent.getSerializableExtra(MainActivity.NOTE_CATEGORY_EXTRA);
-        noteCatButton.setImageResource(Note.categoryToDrawable(noteCat));
 
+        if(savedButtonCategory != null){
+            noteCatButton.setImageResource(Note.categoryToDrawable((savedButtonCategory)));
+        } else {
+            Note.Category noteCat = (Note.Category) intent.getSerializableExtra(MainActivity.NOTE_CATEGORY_EXTRA);
+            savedButtonCategory = noteCat;
+            noteCatButton.setImageResource(Note.categoryToDrawable(noteCat));
+        }
         buildCategoryDialog();
+        buildConfirmDialog();
 
         noteCatButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +76,12 @@ public class NoteEditFragment extends Fragment {
         });
 
         return framentLayout;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+        saveInstanceState.putSerializable(MODIFIED_CATEGORY,savedButtonCategory );
     }
 
     private void buildCategoryDialog(){
@@ -93,4 +121,36 @@ public class NoteEditFragment extends Fragment {
         categoryDialog = categoryBuilder.create();
     }
 
+    private void buildConfirmDialog(){
+        AlertDialog.Builder confirmBuilder = new AlertDialog.Builder(getActivity());
+
+        confirmBuilder.setTitle("Are you sure?");
+        confirmBuilder.setMessage("Are you sure, you want to save the note?");
+
+        confirmBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("Save Note", "Note Title:" + title.getText() +
+                        "Note Message:" + message.getText() +
+                        "Note Category" + savedButtonCategory
+                );
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        confirmBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                    //Do nothing here
+
+            }
+        });
+
+        confirmDialog = confirmBuilder.create();
+    }
+
 }
+
+
